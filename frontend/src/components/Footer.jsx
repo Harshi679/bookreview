b09c832f22681452a169d0dc00f30c32868c0d1b
Harshi679 <rharshitha417@gmail.com>
2025-06-19 13:35:24 +0530

    first commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import { Button } from "./ui/button";
+import { Link } from "react-router-dom";
+import { SiCodechef, SiLeetcode, SiGeeksforgeeks } from "react-icons/si";
+import { FaLinkedinIn, FaYoutube, FaGithub, FaInstagram } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <footer className="py-2 px-4 bg-slate-100 z-30 sm:flex items-center gap-2 border-t border-slate-200  sm:bg-white dark:bg-zinc-950 dark:border-zinc-800">
+      <h1 className="flex justify-center items-center gap-2 pb-2 truncate">
+        <span>Made by</span>
+        <span className="text-xl font-medium tracking-tight truncate transition-colors dark:text-zinc-50">
+          @Harshitha S
+        </span>
+      </h1>
+      <div className="sm:ml-auto flex items-center justify-center gap-2 ">
+        <Link to="https://www.linkedin.com/in/harshitha-s-88a5b4212/?originalSubdomain=in" target="_blank">
+          <Button variant="outline" size="icon">
+            <FaLinkedinIn className="h-6 w-6" />
+            <span className="sr-only">Linkedin</span>
+          </Button>
+        </Link>
+       
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
